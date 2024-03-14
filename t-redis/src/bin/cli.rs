ce6620b0f67bae1bebc4da50f56c48a5aebb0e77
jgx <137034198@qq.com>
2024-03-14 18:24:09 +0800

    tracing

--- t-redis/src/bin/cli.rs
+++ t-redis/src/bin/cli.rs
@@ -1,11 +1,20 @@
-use bytes::Bytes;
-use t_redis::clients::Client;
+use std::env;
+
+// use bytes::Bytes;
+// use t_redis::clients::Client;
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
-    let mut client = Client::connect("127.0.0.1:8888").await.unwrap();
+    env::set_var("RUST_LOG", "info");
+    tracing_subscriber::fmt::init();
 
-    let rs = client.ping(Some(Bytes::from("Ping".as_bytes()))).await;
+    info!("hello");
 
-    println!("ping return {:?}", rs);
+    println!("test log");
+    // let mut client = Client::connect("127.0.0.1:8888").await.unwrap();
+    //
+    // let rs = client.ping(Some(Bytes::from("Ping".as_bytes()))).await;
+    //
+    // println!("ping return {:?}", rs);
 }
