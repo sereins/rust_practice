70f387938c5e80e7815e8092321878dcf0e6fc27
jgx <137034198@qq.com>
2024-03-18 17:14:52 +0800

    tracing 日志

--- t-redis/src/bin/cli.rs
+++ t-redis/src/bin/cli.rs
@@ -1,20 +1,42 @@
+use bytes::Bytes;
 use std::env;
-
-// use bytes::Bytes;
-// use t_redis::clients::Client;
-use tracing::info;
+use t_redis::clients::Client;
+use tracing_appender::{non_blocking::WorkerGuard, rolling::Rotation};
+use tracing_subscriber::fmt::writer::MakeWriterExt;
 
 #[tokio::main]
 async fn main() {
-    env::set_var("RUST_LOG", "info");
-    tracing_subscriber::fmt::init();
+    let _guard = init_log();
+    let mut client = Client::connect("127.0.0.1:8888").await.unwrap();
 
-    info!("hello");
+    let rs = client.ping(Some(Bytes::from("Ping".as_bytes()))).await;
 
-    println!("test log");
-    // let mut client = Client::connect("127.0.0.1:8888").await.unwrap();
-    //
-    // let rs = client.ping(Some(Bytes::from("Ping".as_bytes()))).await;
-    //
-    // println!("ping return {:?}", rs);
+    println!("ping return {:?}", rs);
+}
+
+fn init_log() -> Option<WorkerGuard> {
+    // 是否需要输出到日志
+    let write_file = env::var("WRITE_FILE").unwrap_or("false".to_string());
+
+    let stdout = std::io::stdout;
+
+    let subscriber = tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG);
+    if write_file == "true" {
+        let file_appender = tracing_appender::rolling::RollingFileAppender::builder()
+            .rotation(Rotation::DAILY)
+            .filename_suffix("log")
+            .build("./logs")
+            .expect("build log file appender errro");
+
+        let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+
+        subscriber
+            .with_writer(stdout.and(non_blocking))
+            .with_ansi(false)
+            .init();
+        Some(guard)
+    } else {
+        subscriber.with_writer(stdout).init();
+        None
+    }
 }
